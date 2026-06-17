7d95d20b9f1d7448ca6106bbfe95de8b8d882d09
Gruheem <grahamsilcock91@gmail.com>
2026-06-17 14:18:13 +0100

    Fix Template response in Profiles View

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def profiles(request):
     """ A view to return the profiles page """
-    template = 'profiles/profiles.html'
+    template = 'profiles/profile.html'
     context = {}
 
     return render(request, template, context)
