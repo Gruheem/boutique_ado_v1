88ee774fdba41af6b6927a2a349844014b72cea8
Gruheem <grahamsilcock91@gmail.com>
2026-06-25 13:32:53 +0100

    Deployment Tweaks

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,12 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = [
     'localhost', 
     '127.0.0.1',
-    'boutique-ado-g-65c184f676ee.herokuapp.com/'
+    'boutique-ado-g-65c184f676ee.herokuapp.com',
 ]
 
 
