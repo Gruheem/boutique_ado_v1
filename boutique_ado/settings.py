16b5327896864e9289df959a43e12abe4b2f0ed9
Gruheem <grahamsilcock91@gmail.com>
2026-06-29 11:38:42 +0100

    Settings Change to Try to Make Webhooks Work

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -220,4 +220,4 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ['https://your-app.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://boutique-ado-g-65c184f676ee.herokuapp.com/']
